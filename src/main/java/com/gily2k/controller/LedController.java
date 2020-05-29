dd31c9f0f09bf515bb53cba91a1b4a57b5261665
gily2k <gily2k@gmail.com>
2020-05-29 09:28:25 +0300

    a

--- src/main/java/com/gily2k/controller/LedController.java
+++ src/main/java/com/gily2k/controller/LedController.java
@@ -22,7 +22,7 @@ public class LedController {
             pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
         }
 
-        pin.pulse(4000);
+        pin.toggle();
 
         return GpioFactory.getInstance().getState(pin).toString();
     }
