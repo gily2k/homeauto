0ca649bbbf874a47efd89ecefd1c7316e11fc34c
gily2k <gily2k@gmail.com>
2020-06-06 00:36:18 +0300

    a

--- src/main/java/com/gily2k/tasks/MomentaryButton.java
+++ src/main/java/com/gily2k/tasks/MomentaryButton.java
@@ -8,23 +8,23 @@ import java.util.Date;
 
 @Service
 public class MomentaryButton {
-    final GpioController gpio = GpioFactory.getInstance();
-    GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01,
-            "MyButton",
-            PinPullResistance.PULL_UP);
-
-    GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.LOW);
-
-    @Scheduled(fixedRate=100)
-    public void checkButton() {
-        Date now = new Date();
-        System.out.println(String.format("Button check [%s] button state -> %s", now, myButton.getState()));
-        if (myButton.isHigh()) {
-            myLed.setState(PinState.HIGH);
-        } else {
-            myLed.setState(PinState.LOW);
-        }
-
-    }
+//    final GpioController gpio = GpioFactory.getInstance();
+//    GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01,
+//            "MyButton",
+//            PinPullResistance.PULL_UP);
+//
+//    GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLED", PinState.LOW);
+//
+//    @Scheduled(fixedRate=100)
+//    public void checkButton() {
+//        Date now = new Date();
+//        System.out.println(String.format("Button check [%s] button state -> %s", now, myButton.getState()));
+//        if (myButton.isHigh()) {
+//            myLed.setState(PinState.HIGH);
+//        } else {
+//            myLed.setState(PinState.LOW);
+//        }
+//
+//    }
 
 }
