f5d067e232989f47a818464ea66210e87c69e259
gily2k <gily2k@gmail.com>
2020-06-06 23:14:14 +0300

    a

--- src/main/java/com/gily2k/controller/LedController.java
+++ src/main/java/com/gily2k/controller/LedController.java
@@ -7,19 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LedController {
-    private GpioController gpio = GpioFactory.getInstance();
-    final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+//    private GpioController gpio = GpioFactory.getInstance();
+//    final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
 
     @RequestMapping("/led/blink")
     public String blink() {
-        led.blink(5000, 2000);
         return "blink";
     }
 
     @RequestMapping("/led/toggle")
     public String startListen() {
-        led.toggle();
-        return led.getState().getName();
+        return "led.getState().getName()";
     }
 
 
