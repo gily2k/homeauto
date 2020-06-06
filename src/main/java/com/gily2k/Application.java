9e04200dca340fcff58f08a350bb0104756c3110
gily2k <gily2k@gmail.com>
2020-06-06 22:01:10 +0300

    save

--- src/main/java/com/gily2k/Application.java
+++ src/main/java/com/gily2k/Application.java
@@ -1,8 +1,5 @@
 package com.gily2k;
 
-import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,17 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_01, PinPullResistance.PULL_DOWN);
-
-        myButton.addListener(new GpioPinListenerDigital() {
-            @Override
-            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
-                System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            }
-
-        });
     }
 
 }
--- src/main/java/com/gily2k/controller/LedController.java
+++ src/main/java/com/gily2k/controller/LedController.java
@@ -1,37 +1,26 @@
 package com.gily2k.controller;
 
+
 import com.pi4j.io.gpio.*;
-import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
-import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LedController {
+    private GpioController gpio = GpioFactory.getInstance();
+    final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
 
-
-    @RequestMapping("/")
-    public String greeting() {
-        return "Hello world!";
+    @RequestMapping("/led/blink")
+    public String blink() {
+        led.blink(5000, 2000);
+        return "blink";
     }
 
-    @RequestMapping("/start")
+    @RequestMapping("/led/toggle")
     public String startListen() {
-
-        return "Ok";
+        led.toggle();
+        return led.getState().getName();
     }
 
-//    @RequestMapping("/light")
-//    public String light() {
-//        if (pin == null) {
-//            GpioController gpio = GpioFactory.getInstance();
-//
-//            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
-//        }
-//
-//        pin.toggle();
-//
-//        return GpioFactory.getInstance().getState(pin).toString();
-//    }
 
 }
