2a33589bcf394f9d174ddec7f85ff260ebb2bf61
gily2k <gily2k@gmail.com>
2020-06-06 22:50:02 +0300

    save

---
+++ src/main/java/com/gily2k/inputs/ButtonInput.java
@@ -0,0 +1,22 @@
+package com.gily2k.inputs;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.pi4j.io.gpio.*;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import java.util.Date;
+
+@Controller
+public class ButtonInput {
+    final GpioController gpio = GpioFactory.getInstance();
+    private GpioPinDigitalInput momButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "mom-button", PinPullResistance.PULL_DOWN);
+
+    @Scheduled(fixedRate = 200)
+    public void tickMomButton() {
+        Date now = new Date();
+        System.out.println(String.format("tick [%s] -> $", now.toString(), momButton.getState()));
+    }
+
+
+}
